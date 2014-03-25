6894176663e51dab61820806795cdcf0743f67fa
waln <linwanggm@gmail.com>
2014-03-26 01:08:37 +0800

    Update clusterTest.py

--- script/clusterTest.py
+++ script/clusterTest.py
@@ -4,11 +4,11 @@
 # readme: create table on every coordinator, then isert insertNum rows
 #  ;select all tables on every datanode and standy,at last drop the tables
 #  if the cluster hasnot standby, please comment the statement:
-#   "checkNodeSelect(pt_standby,'standby',insertNum)"
+#   "checkNodeSelect(pt_standby,'standby',insertNum)" 
 ########################################################################################################
 
 #!/usr/bin/evn python
-import os, sys, getpass
+import os, sys, getpass,subprocess
 
 global checkOK
 #### there are some parameters that may need modify as below
@@ -34,17 +34,20 @@ def checkPgxcNode():
     print '#'*15 + ' ' * ((20-len(string))/2) + string + ' ' * ((20-len(string))/2) + '#'*15
     while i < cnNum:
         checkOK = False
-        print pt_coordinator[2*i]
+        print 'ip: ' + pt_coordinator[2*i] + (18-len(pt_coordinator[2*i])) *' ' + 'port: ' + pt_coordinator[2*i+1] 
         cmd = ''
         if len(path) == 0:
             cmd = cmd + path + '/'
         cmd = cmd + "psql -d postgres -p " + pt_coordinator[2*i+1] + " -h " + pt_coordinator[2*i] + " -c 'select count(*) from pgxc_node';"
-        file = os.popen(cmd)
-        returnOut = file.read().split('\n')
+        result=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        if result.stderr.read() != '':
+            print 'Error'
+            return
+        returnOut = result.stdout.read().split('\n')
         if int(returnOut[2]) != nodeNum:
             print pt_coordinator[2*i] + ' : pgxc_node isn\'t right'
             return
-        i = i + 1
+        i = i + 1 
     print '-' *5 + ' pgxc_node check ok. ' + '-' * 5 + '\n'
     checkOK = True
 
@@ -61,13 +64,16 @@ def checkCreateTable():
     print '#'*15 + ' ' * ((20-len(string))/2) + string + ' ' * ((20-len(string))/2) + '#'*15
     while i < cnNum:
         checkOK = False
-        print pt_coordinator[2*i]
+        print 'ip: ' + pt_coordinator[2*i] + (18-len(pt_coordinator[2*i])) *' ' + 'port: ' + pt_coordinator[2*i+1]
         cmd = ''
         if len(path) == 0:
             cmd = cmd + path + '/'
         cmd = cmd + "psql -d postgres -p " + pt_coordinator[2*i+1] + " -h " + pt_coordinator[2*i] + " -c 'create table test_t" + str(i) + "(id int) distribute by replication;'"
-        file = os.popen(cmd)
-        returnOut = file.read().split('\n')
+        result=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        if result.stderr.read() != '':
+            print 'Error'
+            return
+        returnOut = result.stdout.read().split('\n')
         if returnOut[0] != "CREATE TABLE":
             print pt_coordinator[2*i] + ' : create table Error!'
             return
@@ -88,13 +94,16 @@ def checkDropTable():
     print '#'*15 + ' ' * ((20-len(string))/2) + string + ' ' * ((20-len(string))/2) + '#'*15
     while i < cnNum:
         checkOK = False
-        print pt_coordinator[2*i]
+        print 'ip: ' + pt_coordinator[2*i] + (18-len(pt_coordinator[2*i])) *' ' + 'port: ' + pt_coordinator[2*i+1]
         cmd = ''
         if len(path) == 0:
             cmd = cmd + path + '/'
         cmd = cmd + "psql -d postgres -p " + pt_coordinator[2*i+1] + " -h " + pt_coordinator[2*i] + " -c 'drop table test_t" + str(i) + ";'"
-        file = os.popen(cmd)
-        returnOut = file.read().split('\n')
+        result=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        if result.stderr.read() != '':
+            print 'Error'
+            return
+        returnOut = result.stdout.read().split('\n')
         if returnOut[0] != "DROP TABLE":
             print pt_coordinator[2*i] + ' : drop table Error!'
             return
@@ -112,10 +121,10 @@ def checkNodeSelect(nodeList,nodeName,value):
     nodei = 0
     nodeNum = len(nodeList)/2
     cnNum = len(pt_coordinator)/2
-    string = 'check select on'
+    string = 'check select '
     print '#'*15 + ' ' * ((20-len(string))/2) + string + ' ' * ((20-len(string))/2) + '#'*15
     while nodei < nodeNum:
-        print nodeList[2*nodei]
+        print 'ip: ' + nodeList[2*nodei] + (18-len(nodeList[2*nodei])) *' ' + 'port: ' + nodeList[2*nodei+1] 
         tbj = 0
         while tbj < cnNum:
             checkOK = False
@@ -123,8 +132,12 @@ def checkNodeSelect(nodeList,nodeName,value):
             if len(path) == 0:
                 cmd = cmd + path + '/'
             cmd = cmd + "psql -d postgres -p " + nodeList[2*nodei+1] + " -h " + nodeList[2*nodei] + " -c 'select count(1) from test_t" + str(tbj) + ";'"
-            file = os.popen(cmd)
-            returnOut = file.read().split('\n')
+
+            result=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+            if result.stderr.read() != '':
+                print 'Error'
+                return
+            returnOut = result.stdout.read().split('\n')
             if len(returnOut) < 2 or int(returnOut[2]) != value:
                 print pt_datanode[2*nodei] + ' : ' + nodeList + ' select Error!'
                 return
@@ -142,24 +155,27 @@ def checkInsert(value):
         return
     i = 0
     cnNum = len(pt_coordinator)/2
-    string = 'check insert on'
+    string = 'check insert '
     print '#'*15 + ' ' * ((20-len(string))/2) + string + ' ' * ((20-len(string))/2) + '#'*15
     while i < cnNum:
         checkOK = False
-        print pt_coordinator[2*i]
+        print 'ip: ' + pt_coordinator[2*i] + (18-len(pt_coordinator[2*i])) *' ' + 'port: ' + pt_coordinator[2*i+1] 
         cmd = ''
         if len(path) == 0:
             cmd = cmd + path + '/'
         cmd = cmd + "psql -d postgres -p " + pt_coordinator[2*i+1] + " -h " + pt_coordinator[2*i] + " -c 'insert into test_t" + str(i) + " select generate_series(1," + str(value) +");'"
-        file = os.popen(cmd)
-        returnOut = file.read().split('\n')
+        result=subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+        if result.stderr.read() != '':
+            print 'Error'
+            return
+        returnOut = result.stdout.read().split('\n')
         if returnOut[0] != "INSERT 0 " + str(value):
             print pt_coordinator[2*i] + ' : coordinator insert Error!'
             return
         i = i + 1
     print '-' *5 + ' insert ok. ' + '-' * 5 + '\n'
     checkOK = True
-
+    
 ##
 # function noCheckDropTable()
 ##
@@ -178,7 +194,7 @@ def noCheckDropTable():
         if len(path) == 0:
             cmd = cmd + path + '/'
         cmd = cmd + "psql -d postgres -p " + pt_coordinator[2*i+1] + " -h " + pt_coordinator[2*i] + " -c 'drop table test_t" + str(i) + ";'"
-        file = os.popen(cmd)
+        subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
         i = i + 1
 
 
@@ -191,7 +207,7 @@ if __name__ == '__main__':
     cnNum = len(pt_coordinator)
     dnNum = len(pt_datanode)
     if cnNum%2 == 1 or dnNum %2 == 1:
-        print 'alter,node list is wrong.'
+        print 'alter,node list is wrong.' 
         exit(1)
     checkPgxcNode()
     checkCreateTable()
@@ -199,5 +215,5 @@ if __name__ == '__main__':
     checkNodeSelect(pt_coordinator,'coordinator',insertNum)
     checkNodeSelect(pt_datanode,'datanode',insertNum)
     checkNodeSelect(pt_standby,'standby',insertNum)
-    checkDropTable()
-    noCheckDropTable()
+    checkDropTable()  
+    noCheckDropTable() 
